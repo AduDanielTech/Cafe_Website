92b7d7586f4047e2df10cf9ce163be44790f40e6
Fhejs7g <adudaniel097@gmail.com>
2023-08-29 03:10:19 +0100

    solving conflicts

--- functions/api.js
+++ functions/api.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
-
-
-const authRouter = require('./routes/admin/auth');
-const adminProductsRouter = require('./routes/admin/products');
-const productsRouter = require('./routes/products');
-const cartsRouter = require('./routes/carts');
-const bookRouter = require('./routes/book');
-
+const authRouter = require('../routes/admin/auth');
+const adminProductsRouter = require('../routes/admin/products');
+const productsRouter = require('../routes/products');
+const cartsRouter = require('../routes/carts');
+const bookRouter = require('../routes/book');
+const serverless = require('serverless-http');
 const app = express();
-
 
 app.use(express.static('public', { maxAge: '1h' }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,6 +22,6 @@ app.use(productsRouter);
 app.use(cartsRouter);
 app.use(bookRouter);
  
-app.listen(3002, () => {
-  console.log('running server on port: http://localhost:3002')
+app.listen(3000, () => {
+  console.log('running server on port: http://localhost:3000');
 });
