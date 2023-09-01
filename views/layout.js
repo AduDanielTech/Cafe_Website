107abf4d440bbdf4e187b571b4d7bb2ec383cb65
Fhejs7g <adudaniel097@gmail.com>
2023-09-01 13:12:05 +0100

    updating meta_image

--- views/layout.js
+++ views/layout.js
@@ -5,9 +5,9 @@ module.exports = ({ content }) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:image" content="https://github.com/AduDanielTech/Cafe_Website/tree/main/public/image/meta_image.png?raw=true">
         <title>The Gourmet Griddle</title>
             <!-- styles -->
-    
             <!-- bootstrap link -->
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
             <link rel="stylesheet"  type="text/css" href="/css/style.css">
