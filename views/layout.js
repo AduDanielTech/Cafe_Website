837ab11e0b34cbbf24ca0ac9bceabf4474b2ac2c
Fhejs7g <adudaniel097@gmail.com>
2023-09-01 11:16:12 +0100

    sync

--- views/layout.js
+++ views/layout.js
@@ -10,8 +10,9 @@ module.exports = ({ content }) => {
     
             <!-- bootstrap link -->
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-            <link rel="stylesheet" href="/css/style.css">
-    
+            <link rel="stylesheet"  type="text/css" href="/css/style.css">
+          
+
             <!-- Google Fonts -->
             <link rel="preconnect" href="https://fonts.googleapis.com">
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -27,7 +28,7 @@ module.exports = ({ content }) => {
 
     
         <!-- Library Stylesheets -->
-        <link href="/lib/animate/animate.min.css" rel="stylesheet">
+        <link  type="text/css" href="/lib/animate/animate.min.css" rel="stylesheet">
         
         
     </head>
@@ -151,11 +152,11 @@ module.exports = ({ content }) => {
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     
         <!-- Libraries -->
-        <script src="/lib/wow/wow.min.js"></script>
-        <script src="/lib/easing/easing.min.js"></script>
-        <script src="/lib/counterup/counterup.min.js"></script>
+        <script type="application/javascript" src="/lib/wow/wow.min.js"></script>
+        <script type="application/javascript" src="/lib/easing/easing.min.js"></script>
+        <script type="application/javascript" src="/lib/counterup/counterup.min.js"></script>
     
-        <script src="/js/main.js"></script>
+        <script type="application/javascript" src="/js/main.js"></script>
         <script >
         (function ($) {
             "use strict";
