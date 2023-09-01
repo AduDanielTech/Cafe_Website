5ee05f3833af6fb2797e82edc0963561c371a249
Fhejs7g <adudaniel097@gmail.com>
2023-09-01 12:59:36 +0100

    updating mme

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
-  const admin = require('firebase-admin')
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  const cookieSession = require('cookie-session');
+const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const authRouter = require('./routes/admin/auth');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
+const bookRouter = require('./routes/book');
+const path = require('path');
 
+const app = express();
 
-  const authRouter = require('./routes/admin/auth');
-  const adminProductsRouter = require('./routes/admin/products');
-  const productsRouter = require('./routes/products');
-  const cartsRouter = require('./routes/carts');
-  const bookRouter = require('./routes/book');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['lkasld235j'],
+    maxAge: 24 * 60 * 60 * 1000,
+  })
+);
 
-  const app = express();
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Define your routes
+app.use(authRouter);
+app.use(adminProductsRouter);
+app.use(productsRouter);
+app.use(cartsRouter);
+app.use(bookRouter);
 
-  const path = require('path');
+const port = process.env.PORT || 3001;
 
-  app.use(express.static(path.join('./public', 'public')));
-
-
-  app.use(express.static('public', { maxAge: '1h' }));
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(
-    cookieSession({
-      name: 'session',
-      keys: ['lkasld235j'],
-      maxAge: 24 * 60 * 60 * 1000,
-    })
-  );
-
-
-
-  app.use(authRouter); 
-  app.use(adminProductsRouter);
-  app.use(productsRouter);
-  app.use(cartsRouter);
-  app.use(bookRouter);
-
-
-  
-  app.listen(3001, () => {
-    console.log('running server on port: http://localhost:3001')
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port: http://localhost:${port}`);
+});
